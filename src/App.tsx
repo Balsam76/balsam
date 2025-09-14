93f68682badb660c6bece6b5eb029d13ab90d78e
Balsam76 <balsam.ai.help@gmail.com>
2025-09-14 02:10:59 -0700

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import StatsSection from './components/StatsSection';
 import AboutSection from './components/AboutSection';
 import FeaturesSection from './components/FeaturesSection';
 import DownloadSection from './components/DownloadSection';
@@ -14,6 +15,7 @@ function App() {
       <Header mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
       <main>
         <HeroSection />
+        <StatsSection />
         <AboutSection />
         <FeaturesSection />
         <DownloadSection />
